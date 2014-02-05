6516ca04a49108ccd6438eee63ea02fa0205a8b1
Nathan Zadoks <nathan@nathan7.eu>
2014-02-05 15:02:17 +0100

    simplify everything using readable

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ exports = module.exports = BinaryParseStream
 var Stream = require('stream')
   , TransformStream = Stream.Transform
   , inherits = require('util').inherits
+  , Readable = require('readable')
+  , abs = Math.abs
 
-var One = exports.One = -1
+exports.One = -1
 
 inherits(BinaryParseStream, TransformStream)
 function BinaryParseStream(options) {
@@ -12,56 +14,38 @@ function BinaryParseStream(options) {
   this._writableState.objectMode = false
   this._readableState.objectMode = true
 
-  this._queue = null
-  this._needed = 0
-  this._single = false
-  this._parser = this._parse()
+  this.__read = Readable(Buffer(0))
+  this.__restart()
 }
 
-BinaryParseStream.prototype._transform = function(fresh, encoding, cb) {
-  var queue = this._queue === null
-    ? fresh
-    : Buffer.concat(this._queue, fresh)
+BinaryParseStream.prototype._transform = function(fresh, encoding, cb) { var self = this
+  this.__read.push(fresh)
 
-  var needed = this._needed
-    , single = this._single
-
-  while (queue.length >= needed) {
-    var chunk = queue.slice(0, needed)
-    queue = queue.slice(needed)
-    
+  while (this.__read.able >= abs(this.__needed)) {
     var ret
-      , err
-    try {
-      if (single)
-        ret = this._parser.next(chunk[0])
-      else
-        ret = this._parser.next(chunk)
-    }
+      , chunk = this.__read(this.__needed)
+
+    try { ret = this.__parser.next(chunk) }
+
     catch (e) {
-      err = e
-      ret = { done: true }
+      this.emit('error', e)
+      this.__restart()
+      continue
+    }
+    
+    if (ret.done) {
+      this.push(ret.value)
+      this.__restart()
+      continue
     }
 
-    if (!ret.done) {
-      var value = ret.value
-      single = value === One
-      needed = single
-        ? 1
-        : value
-    }
-    else {
-      if (err)
-        this.emit('error', err)
-      else
-        this.push(ret.value)
-      needed = 0
-      this._parser = this._parse()
-    }
+    this.__needed = ret.value
   }
 
-  this._queue = queue
-  this._needed = needed
-  this._single = single
   return cb()
+}
+
+BinaryParseStream.prototype.__restart = function() {
+  this.__needed = 0
+  this.__parser = this._parse()
 }
